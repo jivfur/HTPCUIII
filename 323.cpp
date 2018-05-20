5b09e979aa03d7e083a2a78e3f62eee2965cac11
jivfur <jivfur@tamu.edu>
2018-05-20 13:59:29 -0500

    323 Como programar en C/C++

---
+++ 323.cpp
@@ -0,0 +1,18 @@
+/*
+author: jivfu
+Date: 2018-05-20
+Descripcion: Ejercicio 3.23 del Libro como programar en C/c++ D&D
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int cont = 1;
+	while (cont < 11) {
+		printf("%d\t", cont);
+		cont++;
+	}
+	system("pause");
+	return 0;
+}
